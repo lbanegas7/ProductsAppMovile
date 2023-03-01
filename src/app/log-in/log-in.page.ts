a6ed3f103f7838c9e9ced146c3dd615032929d0f
Luis Banegas <luisbanegas06@gmail.com>
2023-02-28 23:35:55 -0600

    add login

---
+++ src/app/log-in/log-in.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.page.html',
+  styleUrls: ['./log-in.page.scss'],
+})
+export class LogInPage implements OnInit {
+  
+  loginError = false;
+
+  formLogin = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" )]],
+    password: ['', [Validators.required]]
+  })
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private route: Router, private loadingLogin: LoadingController) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    if(this.formLogin.valid){
+
+      this.LoadingSession();
+
+      const { email, password} = this.formLogin.value
+      // Verificamos las credenciales
+      this.authService.loginUser(email! , password!)
+      .then(userCredencial => {
+        this.loadingLogin.dismiss();
+        this.loginError = false;
+        this.formLogin.reset();
+        this.route.navigate(['./home']);
+      })
+      .catch(error => {
+        this.formLogin.reset();
+        this.formLogin.clearValidators();
+        this.loginError = true;
+      })
+      
+    }
+  }
+
+  async LoadingSession(){
+    const loading = await this.loadingLogin.create({
+      message: 'Iniciando sesión',
+      spinner: 'circles',
+      mode: "ios"
+    });
+
+    return await loading.present();
+  }
+
+  get email(){
+    return this.formLogin.get('email')
+  }
+  
+  get password(){
+    return this.formLogin.get('password')
+  }
+}
