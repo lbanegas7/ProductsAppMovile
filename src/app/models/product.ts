4b763251be90f5a06bb2e518abad3dce5708665c
Luis Banegas <luisbanegas06@gmail.com>
2023-02-28 23:37:19 -0600

    add models

---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    nombre: string,
+    precio: number,
+    descripcion: string,
+    imagen: string,
+    unidadMedida: string,
+    cantidadStock: number,
+    notificarBajoStock: number,
+    estado: string
+}
