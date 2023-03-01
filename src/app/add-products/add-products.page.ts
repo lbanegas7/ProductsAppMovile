cf4a54c8eafd97330851a1adceeb62a74a42c4c5
Luis Banegas <luisbanegas06@gmail.com>
2023-02-28 23:35:44 -0600

    Create add-products.page.ts

---
+++ src/app/add-products/add-products.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-products',
+  templateUrl: './add-products.page.html',
+  styleUrls: ['./add-products.page.scss'],
+})
+export class AddProductsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
