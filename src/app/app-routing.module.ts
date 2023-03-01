19f52d762b13ca5c1f66f81575f9a4a0628059c9
Luis Banegas <luisbanegas06@gmail.com>
2023-03-01 05:58:15 -0600

    implements guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import {UserGuard} from './guards/user.guard';
+import {DefaultGuard} from './guards/default.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -8,18 +10,22 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    canActivate: [DefaultGuard],
     path: 'log-in',
     loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule)
   },
   {
+    canActivate: [UserGuard],
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    canActivate: [UserGuard],
     path: 'products',
     loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
   },
   {
+    canActivate: [UserGuard],
     path: 'add-products',
     loadChildren: () => import('./add-products/add-products.module').then( m => m.AddProductsPageModule)
   },
